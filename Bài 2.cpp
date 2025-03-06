ae5103ed06c81d712e52afa38a0fba631a9d5281
Tantu-It <nguyentantu612@gmail.com>
2025-03-06 22:09:59 +0700

    bai 2

---
+++ Bài 2.cpp
@@ -0,0 +1,40 @@
+﻿/*#include<iostream>
+using namespace std;
+
+int main()
+{
+    // Nhập từ bàn phím
+    double cd, cr, cv, dt;
+
+    // Nhập chiều dài
+    cout << "Chieu dai: ";
+    cin >> cd;
+
+    // Nhập chiều rộng
+    cout << "Chieu rong: ";
+    cin >> cr;
+
+    // Tính chu vi
+    cv = (cd + cr) * 2;
+    cout << "Chu vi la: " << cv << endl;  // In chu vi ra, sau đó dùng endl để xuống dòng
+
+    // Tính diện tích
+    dt = cd * cr;
+    cout << "Dien tich la: " << dt << endl;  // In diện tích ra và xuống dòng
+
+    return 0;
+}
+*/
+//Bài tập vận dụng 01 
+#include<iostream>
+using namespace std; 
+int main()
+{
+    cout << " Nguyen Tan Tu " << "\n";
+
+    cout << " 06/12/2005 " << "\n";
+
+    cout << " Nam "<< "\n";
+
+    cout << " 18 " << "\n" ;
+}
